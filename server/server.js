3edb23ec4be463f5c7a677c3e9d91f5020fdf369
MademVenkateswaraReddy <venkateswara.madem@gmail.com>
2024-01-12 13:02:06 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -8,20 +8,14 @@ import connect from './database/conn.js'
 const app = express()
 
 app.use(morgan('tiny'))
-app.use(cors(
-    {
-        origin: ["http://deploy-mern-quiz-vercel-app"],
-        methods: ['POST', 'GET', 'DELETE'],
-        credentials: true
-    }
-))
+app.use(cors())
 app.use(express.json())
 config();
 const port = process.env.PORT || 8080
 
 app.use('/api', router)
 
-app.get('*', (req, res)=>{
+app.get('/', (req, res)=>{
     try {
         res.json('Get Request')
     } catch (error) {
